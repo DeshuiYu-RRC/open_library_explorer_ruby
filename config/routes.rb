ba88a5be2b1c712cf90d44f0cc30ece47c52dd8d
DeshuiYu-RRC <dyu3@rrc.ca>
2025-11-02 13:37:58 -0600

    create controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get "pages/about"
+  get "subjects/index"
+  get "subjects/show"
+  get "authors/index"
+  get "authors/show"
+  get "books/index"
+  get "books/show"
+  get "books/search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
